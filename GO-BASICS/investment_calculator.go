342769587607fb0cf57a29622874ad7cf44c8174
AngelTravieso <angeltraviesoc@gmail.com>
2024-03-13 17:42:19 -0400

    Understanding the importance of variables

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -10,9 +10,12 @@ func main() {
 	const inflationRate = 2.5
 
 	// investmentAmount = 1000, years = 10
-	var investmentAmount float64 = 1000
+	var investmentAmount float64 // if no have value there a null value (0.00 for float64)
 	years := 10.0
 	expectedReturnRate := 5.5
+
+	// fmt.Scan to take user input
+	fmt.Scan(&investmentAmount) // & -> pointers
 
 	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
