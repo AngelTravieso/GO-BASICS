466881c7e236d14df01f44bf964ffd52a1175722
AngelTravieso <angeltraviesoc@gmail.com>
2024-03-13 16:40:37 -0400

    Making Sense of Constant Values ('Constants')

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -7,13 +7,17 @@ import (
 
 func main() {
 
+	const inflationRate = 2.5
+
 	// investmentAmount = 1000, years = 10
-	var investmentAmount float64 = 1000.0
+	var investmentAmount float64 = 1000
 	years := 10.0
 	expectedReturnRate := 5.5
 
 	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
