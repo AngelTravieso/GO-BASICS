d458fd00797f4c9dc8a13390327d9763e71bfc32
AngelTravieso <angeltraviesoc@gmail.com>
2024-03-14 10:32:25 -0400

    Improved User Input Fetching

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -11,11 +11,19 @@ func main() {
 
 	// investmentAmount = 1000, years = 10
 	var investmentAmount float64 // if no have value there a null value (0.00 for float64)
-	years := 10.0
+	var years float64
 	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
 
 	// fmt.Scan to take user input
 	fmt.Scan(&investmentAmount) // & -> pointers
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
